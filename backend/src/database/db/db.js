3cd4fcb18eca5a2ac69097e038744b2e463e43bc
Diego <diego.gonzaleztoledano@riberadeltajo.es>
2026-04-20 16:14:48 +0200

    Swagger y front

--- backend/src/database/db/db.js
+++ backend/src/database/db/db.js
@@ -1,6 +1,7 @@
-const { Sequelize } = require('sequelize');
 require('dotenv').config();
-console.log('DIALECT:', process.env.DB_DIALECT);
+const { Sequelize } = require('sequelize');
+
+
 const sequelize = new Sequelize(
 	process.env.DB_NAME,
 	process.env.DB_USER,
